779b62cde7e16b996a7d9a19b11f7ae8a6e2b9d4
Toby Baratta <barattat17@grinnell.edu>
2019-10-24 23:44:11 -0700

    Moving g7 logo + splitting text
    Moved g7 logo to go up instead of to the side, added strokes + shadows, split text

--- overlay.js
+++ overlay.js
@@ -1,38 +1,44 @@
 $(document).ready(() => { 
     const entrance = new TimelineMax();
-    // entrance
-    //     .fromTo('.g7-logo', 2, {
-    //         drawSVG:"0 5%"
-    //     }, {
-    //         drawSVG: "20% 80%"
-    //     }, 3);
- 
-    //var sheen = document.getElementById('sheen');
-    //TweenMax.fromTo(sheen, 1, {backgroundPosition:"-200px 0"}, {backgroundPosition:"20px 0"}); 
-    
+
     // Entrance Animation
     TweenMax.to($('.g7-svg'), .4, {opacity: 1, delay: 0.5});
 
-    /* Repeat Animations */
     var tl =  new TimelineMax({ repeatDelay: 10, repeat: -1});
-    mySplitText = new SplitText(".command", {type:"words,chars"}), 
-    chars = mySplitText.chars;
+
+    /* Split Texts for Typewriter Animation */
+    let venueSplit = new SplitText(".venue", {type:"words,chars"}), 
+    venueChars = venueSplit.chars;
+
+    let dateSplit = new SplitText(".date", {type:"words,chars"}), 
+    dateChars = dateSplit.chars;
+
+    let infoSplit = new SplitText(".moreinfo", {type:"words,chars"}),
+    infoChars = infoSplit.chars;
 
     // BEGIN REPEAT ANIMATION
     tl.from('.g7-svg', .5, {
-        x: "+=400",
+        y: "+=40",
         ease: Power2.easeOut
     }, 3)
-    tl.staggerFrom(chars, 0.01, {opacity:0, ease:Power1.easeIn}, 0.08, "+=.25");
-    tl.add(TweenMax.to($('#sheen'), 2.5, {ease: Power3.easeInOut, backgroundPosition: "50px 0"}), "+=4");
-    tl.staggerTo(chars, 0.01, {opacity:0, ease:Power1.easeOut}, -0.08, "+=8");
+    
+    tl.staggerFrom(dateChars, 0.01, {opacity:0, ease:Power1.easeIn}, 0.08, "+=.25");
+    tl.add(TweenMax.to($('#sheen'), 2.5, {ease: Power3.easeInOut, backgroundPosition: "50px 0"}), "+=0.25");
+    tl.staggerTo(dateChars, 0.01, {opacity:0, ease:Power1.easeOut}, -0.06, "+=1.5");
+
+    tl.staggerFrom(venueChars, 0.01, {opacity:0, ease:Power1.easeIn}, 0.08, "+=.25");
+    tl.staggerTo(venueChars, 0.01, {opacity:0, ease:Power1.easeOut}, -0.06, "+=1.5");
+
+    tl.staggerFrom(infoChars, 0.01, {opacity:0, ease:Power1.easeIn}, 0.08, "+=.25");
+    tl.add(TweenMax.to($('#sheen'), 2.5, {ease: Power3.easeInOut, backgroundPosition: "50px 0"}), "+=2");
+    tl.staggerTo(infoChars, 0.01, {opacity:0, ease:Power1.easeOut}, -0.08, "+=6");
+
     tl.to('.g7-svg', .5, {
-        x: "+=400",
+        y: "+=40",
         ease: Power2.easeOut
     }, "+=1")
     tl.add(TweenMax.to($('#sheen'), 2.5, {ease: Power3.easeInOut, backgroundPosition: "50px 0", repeat: 3, repeatDelay: 5}));
     // END REPEAT ANIMATION
-
 
     if (window.obsstudio) {
         window.obsstudio.onActiveChange = (active) => {
